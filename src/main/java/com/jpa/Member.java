3dfad2b2680666c52fa816eac773f3614296f0fd
acisliver <7273322@naver.com>
2022-06-13 13:28:48 +0900

    JPA 기본 사용

---
+++ src/main/java/com/jpa/Member.java
@@ -0,0 +1,29 @@
+package com.jpa;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Member {
+
+    @Id
+    private long id;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
+}
