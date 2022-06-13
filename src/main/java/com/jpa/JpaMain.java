759cf83b3dce436b0733b6f7ed6db68b79f50e47
acisliver <7273322@naver.com>
2022-06-13 13:33:02 +0900

    JPQL 기본 사용

--- src/main/java/com/jpa/JpaMain.java
+++ src/main/java/com/jpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
 
@@ -15,11 +16,11 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
-            Member member = new Member();
-            member.setId(1L);
-            member.setName("MemberA");
-
-            em.persist(member);
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                            .getResultList();
+            for (Member member : result) {
+                System.out.println("member.name = " + member.getName());
+            }
 
             tx.commit();
         } catch (Exception e) {
